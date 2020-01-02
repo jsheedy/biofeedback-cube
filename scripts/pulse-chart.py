3332f7dd8c7f145b40f7b3a8e4390deef543e2ce
Joseph Sheedy <joseph.sheedy@gmail.com>
2020-01-01 16:17:05 -0800

    add pulse-chart, more touchosc modes

---
+++ scripts/pulse-chart.py
@@ -0,0 +1,55 @@
+"""
+realtime plot OSC pulse 
+"""
+
+import argparse
+import asyncio
+import collections
+import math
+
+from matplotlib import pyplot as plt
+
+from pythonosc.dispatcher import Dispatcher
+from pythonosc import osc_server
+
+
+BUF_LEN = 80
+buffer = collections.deque([0]*BUF_LEN, BUF_LEN)
+
+
+def pulse_handler(addr, val):
+    buffer.append(val)
+    plot()
+
+
+def plot():
+    # plt.clf()
+    plt.plot(buffer)
+    plt.draw()
+    plt.pause(0.001)
+    # plt.show()
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="0.0.0.0", help="The ip to listen on")
+    parser.add_argument("--port", type=int, default=37339, help="The port to listen on")
+    args = parser.parse_args()
+
+    plt.ion()
+    # plt.show()
+
+    dispatcher = Dispatcher()
+    dispatcher.map("/pulse", pulse_handler)
+
+    loop = asyncio.get_event_loop()
+    server = osc_server.AsyncIOOSCUDPServer( (args.ip, args.port), dispatcher, loop)
+    # server = osc_server.ThreadingOSCUDPServer( (args.ip, args.port), dispatcher)
+    # server.serve()
+    transport, protocol = await server.create_serve_endpoint()
+    await asyncio.sleep(10**30)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
