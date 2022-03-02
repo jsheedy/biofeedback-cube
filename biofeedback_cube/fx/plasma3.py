9c5bcc053b635c3f521158572e026ad7a046eb51
Joseph Sheedy <joseph.sheedy@gmail.com>
2022-03-02 08:14:56 -0800

    ctrl

--- biofeedback_cube/fx/plasma3.py
+++ biofeedback_cube/fx/plasma3.py
@@ -11,15 +11,15 @@ yy, xx = np.mgrid[0:1:complex(0, HEIGHT), 0:1:complex(0, WIDTH)]
 def plasma3(grid, t):
     palette = index_dict(palettes, hydra.a)
 
-    tau = t * hydra.a
-    f2 = .2 + hydra.b * 5
-    f3 = .2 + hydra.c * 5
-    f1 = .2 + hydra.d * 5
+    tau = 10 * t * hydra.b
+    f1 = .1 + hydra.c * 5
+    f2 = .1 + hydra.d * 5
+    f3 = .1 + hydra.e * 5
 
     field = sin(
-        hydra.x * np.sin(2 * np.pi * f1 * yy + .25 * tau)
-        + hydra.y * np.sin(2 * np.pi * f2 * xx + .6 * tau)
-        + hydra.e * np.sin(10 * xx * f3 * yy + .41 * tau)
+        hydra.x * np.sin(f1 * yy + .2 * tau)
+        + hydra.y * np.sin(f2 * xx + .4 * tau)
+        + hydra.f * np.sin(f3 * xx * yy + .6 * tau)
     )
 
     grid[:, :] = cmap(palette, field)
