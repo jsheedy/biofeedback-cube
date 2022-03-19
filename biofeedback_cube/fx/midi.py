5f62d1a4706028c0e22c8e684a57b51192b0ef58
Joseph Sheedy <joseph.sheedy@gmail.com>
2022-03-19 14:20:28 -0700

    bugfix

--- biofeedback_cube/fx/midi.py
+++ biofeedback_cube/fx/midi.py
@@ -12,10 +12,11 @@ def midi(grid, t):
     dynamic_range = _max - _min
 
     # melody
-    for i, (note, velocity, timestamp) in enumerate(notes[0]):
-        y = (note - _min) / dynamic_range
-        yi = int(y * (HEIGHT - 1))
-        grid[yi, WIDTH-1-i] = (0.0, 1.0, 0)
+    if notes[0]:
+        for i, (note, velocity, timestamp) in enumerate(notes[0]):
+            y = (note - _min) / dynamic_range
+            yi = int(y * (HEIGHT - 1))
+            grid[yi, WIDTH-1-i] = (0.0, 1.0, 0)
 
     # metronome
     if notes[1]:
